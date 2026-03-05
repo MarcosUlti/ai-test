6991a91d5fc3588bf8d12126d4d3ed82a5bb1861
Marcos (Ulti) <70665524+UltimateStrength@users.noreply.github.com>
2026-03-05 20:03:50 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static("public"))
 
-const API_KEY = "apf_itbwvlrrz9o42dyqdwqlz064"
+const API_KEY = process.env.API_KEY
 
 app.post("/chat", async (req, res) => {
 
@@ -34,6 +34,8 @@ app.post("/chat", async (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server rodando em http://localhost:3000")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log("Server rodando")
 })
